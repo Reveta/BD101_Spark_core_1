9b68633d69e01c0bbffa456b403afd570ced8a9b
martaKlachko <booklover93182167@gmail.com>
2018-12-11 17:53:44 +0200

    added code for task1(using data frames)

--- src/main/scala/com/epam/spark/Main.scala
+++ src/main/scala/com/epam/spark/Main.scala
@@ -1,21 +1,25 @@
 package com.epam.spark
 
 
-import org.apache.spark.sql.{SQLContext, SparkSession}
+import org.apache.spark.sql.{DataFrame, SQLContext, SparkSession}
 
 
 object Main extends App  {
-  val spark = SparkSession.builder().master("local").getOrCreate()
-  var trainDF = spark.read.format("csv").
+  val spark: SparkSession = SparkSession.builder().master("local").getOrCreate()
+  var trainDF: DataFrame = spark.read.format("csv").
     option("header", "true").
     option("delimiter", ",").
     load("src\\main\\resources\\train.csv")
 
   //trainDF.show(5)
-  trainDF.createOrReplaceTempView("train")
-  val sqlDF = spark.sql("SELECT  hotel_continent,hotel_country,hotel_market,count(*) as count \nFROM train" +
-    "\nGROUP BY hotel_continent,hotel_country,hotel_market\nORDER BY count desc\nLIMIT 3")
-  sqlDF.show()
+//  trainDF.createOrReplaceTempView("train")
+//  val sqlDF: DataFrame = spark.sql("SELECT  hotel_continent,hotel_country,hotel_market,count(*) as count \nFROM train" +
+//    "\nGROUP BY hotel_continent,hotel_country,hotel_market\nORDER BY count desc\nLIMIT 3")
+//  sqlDF.show()
 
+  trainDF.groupBy("hotel_continent","hotel_country","hotel_market" ).
+    count()
+    .orderBy(org.apache.spark.sql.functions.col("count").desc).limit(3)
+    .show()
 
 }

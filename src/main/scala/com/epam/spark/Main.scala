c4a8bf53fff4ccc91d7938d7b0196bde3acb196c
martaKlachko <booklover93182167@gmail.com>
2018-12-11 13:22:08 +0200

    added code for task1

--- src/main/scala/com/epam/spark/Main.scala
+++ src/main/scala/com/epam/spark/Main.scala
@@ -1,51 +1,21 @@
 package com.epam.spark
 
-import java.util.Date
 
-import org.apache.spark.SparkContext
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SQLContext, SparkSession}
 
-/**
-  * start point*/
-object Main extends App with Engine {
 
-  println(args.mkString("\n"))
+object Main extends App  {
+  val spark = SparkSession.builder().master("local").getOrCreate()
+  var trainDF = spark.read.format("csv").
+    option("header", "true").
+    option("delimiter", ",").
+    load("src\\main\\resources\\train.csv")
 
-  //resolve input argument
-  val (master: String, file: String, saveFile: String) = args.toList match {
-    case firstArg :: secondArg :: thirdArg :: Nil => (firstArg, secondArg, thirdArg)
-    case firstArg :: Nil => (firstArg, "gs://jar_storage/countries_of_the_world.csv", "./" + new Date().getTime)
-    case Nil => ("local", "gs://jar_storage/countries_of_the_world.csv", "./" + new Date().getTime)
-  }
+  //trainDF.show(5)
+  trainDF.createOrReplaceTempView("train")
+  val sqlDF = spark.sql("SELECT  hotel_continent,hotel_country,hotel_market,count(*) as count \nFROM train" +
+    "\nGROUP BY hotel_continent,hotel_country,hotel_market\nORDER BY count desc\nLIMIT 3")
+  sqlDF.show()
 
-  private val sparkContext: SparkContext = SparkSession
-    .builder()
-    .appName("SparkSessionZipsExample")
-    .master(master)
-    .getOrCreate().sparkContext
 
-  private val inputRDD: RDD[String] = sparkContext.textFile(file)
-
-  private val noHeaderRDD: RDD[String] = inputRDD.filter(!isHeaderCsv(_))
-
-  //get region, population and density from csv file
-  private val parsedRDD: RDD[(String, Long, Double)] = noHeaderRDD.map(getCaseRegionIPopultionIpopDensity)
-
-  //Show to console sum of population
-  //println("Population total: " + getPopulationSum(parsedRDD))
-
-  //Show Population Density at region
-  println("Regional Population Density:")
-  val regionsRDD: RDD[(String, Double)] = parsedRDD.map { case (x1, x2, x3) => (clearKey(x1), x3)
-  }.reduceByKey(_ + _)
-
-  val resultStringRDD: RDD[String] = regionsRDD.map(_.toString)
-
-  //resultStringRDD.saveAsTextFile(saveFile)
-  resultStringRDD.collect().foreach(println)
-
-  // while (true) {
-  //   Thread.sleep(10000)
-  // }
 }

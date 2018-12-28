8e5d3dab8f6bcec099dfb48b9c1f26005d26ea78
Roman_Pankiv <PankivRomanMain@gmail.com>
2018-12-28 15:44:27 +0200

    Add Hotel object and class

--- src/main/scala/com/epam/spark/Hotel.scala
+++ src/main/scala/com/epam/spark/Hotel.scala
@@ -1,9 +1,12 @@
 package com.epam.spark
 
+import java.io.StringReader
+import java.util
+
 import org.apache.spark.sql.SparkSession
 
 
-class Hotel(sp: SparkSession, srch_adults_cntI: String, hotel_continentI: String, hotel_countryI: String, hotel_marketI: String, hotel_clusterI: String) extends Serializable{
+case class Hotel(sp: SparkSession, srch_adults_cntI: String, hotel_continentI: String, hotel_countryI: String, hotel_marketI: String, hotel_clusterI: String) {
   private var spark: SparkSession = sp
 
   var srch_adults_cnt: String = srch_adults_cntI
@@ -12,7 +15,39 @@ class Hotel(sp: SparkSession, srch_adults_cntI: String, hotel_continentI: String
   var hotel_market: String = hotel_marketI
   var hotel_cluster: String = hotel_clusterI
 
+}
 
-  override def toString = s"Hotel($srch_adults_cnt, $hotel_continent, $hotel_country, $hotel_market, $hotel_cluster)"
+object Hotel {
 
+  val hotel_continent = 18
+  val hotel_country = 19
+  val hotel_market = 20
+  val srch_adults_cnt = 13
+  val srch_children_cnt = 14
+  val is_booking = 21
+  val user_location_country = 3
+  val srch_destination_id = 16
+
+
+  def isHeaderCsv(line: String): Boolean = line.startsWith("date_time,site_name")
+
+  def apply(row: String): Hotel = {
+    var parser: util.ArrayList[String] = null
+    try {
+      parser = new Parser().parseLine(new StringReader(row))
+    } catch {
+      case e: Exception => print("Exception!")
+    }
+
+    val getCol: Int => String = parser.get _
+    //    val toInt: String => Int =
+    return new Hotel(
+      SparkConfiguration.sparkSession,
+      (getCol(1)),
+      (getCol(4)),
+      (getCol(5)),
+      (getCol(6)),
+      (getCol(7)))
+
+  }
 }
--- src/main/scala/com/epam/spark/Main.scala
+++ src/main/scala/com/epam/spark/Main.scala
@@ -6,17 +6,12 @@ import org.apache.spark.rdd.RDD
 object Main extends App {
 
   val sparkSession = SparkConfiguration.sparkSession
-  var trainRDD: RDD[String] = sparkSession.sparkContext.textFile("src/resorses/train.csv")
+  var trainRDD: RDD[String] = sparkSession.sparkContext.textFile("src\\main\\scala\\resources\\train.csv")
 
-  private val value: RDD[Hotel] = trainRDD.map(a => HotelBuilder.createHotel(a))
-  private val noHeaderRDD: RDD[String] = value.filter(!HotelBuilder.isHeaderCsv(_))
-
-
-
+  private val value: RDD[Hotel] = trainRDD.map(Hotel(_))
 
   println(value.count())
   println(value.first())
-
 
 
   sparkSession.close()

45d4a3d806c7f275786b0c20dc215117d20cb4e9
martaKlachko <booklover93182167@gmail.com>
2018-12-31 16:09:52 +0200

    added task1

--- src/main/scala/com/epam/spark/Main.scala
+++ src/main/scala/com/epam/spark/Main.scala
@@ -6,21 +6,33 @@ import org.apache.spark.rdd.RDD
 object Main extends App {
 
   val sparkSession = SparkConfiguration.sparkSession
-  var trainRDD: RDD[String] = sparkSession.sparkContext.textFile("src\\main\\scala\\resources\\train.csv")
+  var trainRDD: RDD[String] = sparkSession
+    .sparkContext
+    .textFile("src\\main\\scala\\resources\\train.csv")
+
+  val splitted_csv_recordsRDD: RDD[Array[String]] = trainRDD.
+    filter(a => !Hotel.isHeaderCsv(a))
+    .map(line => line.split(",", -1))
+
+  val hotelsRDD: RDD[Hotel] = splitted_csv_recordsRDD
+    .map((line) => Hotel.createHotel(line))
 
 
-  // .filter(a => !Hotel.isHeaderCsv(a))
+  val task1RDD: RDD[(String,Int)] = hotelsRDD.filter(_!=null)
+    .filter(a => a.srch_adults_cnt.equals("2"))
+    .groupBy(_.hotel_continent)
+   .map(kv => (kv._1, kv._2.size))
+    .sortBy(_._2, false)
 
 
-  val splitted_csv_recordsRDD: RDD[Array[String]] = trainRDD.map(line => line.split(",",-1))
+
+  task1RDD.foreach(println)
 
 
-
-  val avocadosRDD = splitted_csv_recordsRDD.map((line) => Hotel.createHotel(line))
-  avocadosRDD.take(20).foreach(println)
-
-
-  //val avocadosRDD: RDD[Hotel] = splitted_csv_recordsRDD.map(l=>Hotel(l))
+  // private val topHotelsDuoRDD: RDD[(Int, Int)] = hotelDuoRDD
+  //    .groupBy(_.hotel_continent)
+  //    .map(kv => (kv._1, kv._2.size))
+  //    .sortBy(_._2, false)
 
 
   sparkSession.close()
